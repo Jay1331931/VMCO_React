43cfb0fe14e2322b0fa5054da0397945443f2b03
sunil.kumar <sunil.kumar@abs.in>
2025-06-02 13:00:24 +0530

    fix: Enhance isE method to include approvalWF parameter for improved field editability logic

--- src/utilities/rbac.js
+++ src/utilities/rbac.js
@@ -1119,11 +1119,11 @@ class RbacManager {
    * @param {string} field - The field name
    * @returns {boolean} True if the field is editable, false otherwise
    */
-  isE(field, fieldInWF = false) {
+  isE(field, approvalWF = false, fieldInWF = false) {
     //console.log(`~~~~~~~~*********Checking access for field: ${field} in form: ${this.currentForm} for role: ${this.currentRole}`);
     const access = this.getFieldAccess(field);
     //console.log(`~~~~~~~~*********Access for field: ${field} is ${JSON.stringify(access)}`);  
-    return (access == null ? true : access.editable) && (this.itemInWF == null ? true : this.isUserOwner) && (fieldInWF == true ? true : false);
+    return (access == null ? true : access.editable) && (this.itemInWF == null ? true : this.isUserOwner) && ((approvalWF == true && fieldInWF == true) ? true : (approvalWF == true && fieldInWF == false) ? false : true);
   }
 }
 
