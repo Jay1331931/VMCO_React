8ccbabc63c199796779afd9fdd7f2ef26208e8a1
ManojGade <gade.manoj@ABS.in>
2025-09-26 15:24:33 +0530

    fixed uatURL blank issue  for remove SO mandatory

--- src/screens/login.js
+++ src/screens/login.js
@@ -4,21 +4,25 @@ import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../i18n';
 import { faLanguage } from '@fortawesome/free-solid-svg-icons';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+
 
 function LoginScreen() {
   const { t, i18n } = useTranslation();
   const location = useLocation();
+    const navigate = useNavigate();
   useEffect(() => {
     document.title = t('Login');
   }, [t]);
+
   const isRTL = i18n.language === 'ar';
   const path = location.pathname;
   const isCustomerLogin = path === '/login' || path === '/';
 
   const title = isCustomerLogin ? t('Customer Login') : t('Employee Login');
   const userType = isCustomerLogin ? 'customer' : null;
-
+ 
   const toggleLanguage = () => {
     const newLang = isRTL ? 'en' : 'ar';
     i18n.changeLanguage(newLang);
