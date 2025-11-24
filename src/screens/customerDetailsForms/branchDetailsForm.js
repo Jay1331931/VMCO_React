c67a1a3f8d7355c1e6770d5e234d9da4f5e308d0
Jay-abs <jay.shah@abs.in>
2025-11-24 11:51:17 +0530

    Updated mandatory fields on branch save

--- src/screens/customerDetailsForms/branchDetailsForm.js
+++ src/screens/customerDetailsForms/branchDetailsForm.js
@@ -788,7 +788,59 @@ let branchdata;
         // UPDATE existing branch
         // console.log("branchPayload:", branchPayload);
         try {
+      const response = await fetch(
+        `${API_BASE_URL}/customer-contacts/branch/${id}/customer/${customer?.id}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" ,
+                                'Authorization': `Bearer ${token}`},
+          
+        }
+      );
+
+      const result = await response.json();
+
+      if (Object.keys(result?.data).length === 0) {
+        // setBranchContacts(result.data);
+        try {
+              const res = await fetch(
+                `${API_BASE_URL}/customer-contacts/create/customer/${customer.id}/branch/${id}`,
+                {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" ,
+                                'Authorization': `Bearer ${token}`},
+                  body: JSON.stringify({
+                    ...updatedBranchContactsData.current,
+                    customer_id: customer.id,
+                    branch_id: id,
+                  }),
+                  
+                }
+              );
+              if (res.ok) {
+                const contactResult = await res.json();
+                if (action !== "submit") {
+                  Swal.fire({
+                    icon: "success",
+                    title: t("Success"),
+                    text: t("Branch saved successfully."),
+                    confirmButtonText: t("OK"),
+                  });
+                }
+                // alert(
+                //   "Branch saved successfully"
+                // );
+              }
+            } catch (error) {
+              console.error("Error saving contacts for new branch:", error);
+            }
+      }
+    } catch (err) {
+      console.error("Error fetching contacts:", err);
+    }
+        try {
           // if (Object.keys(branchPayload).length > 0) {
+
           const response = await fetch(
             `${API_BASE_URL}/customer-branches/id/${id}`,
             {
