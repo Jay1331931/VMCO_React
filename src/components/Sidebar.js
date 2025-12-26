5da9fbe6d74bcaabe3625ed52f61338a904119ca
Ashwin Sridhar <ashwin.sridhar@abs.in>
2025-12-26 11:07:50 +0530

    Style of the main content (padding=0) commented in sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -838,9 +838,9 @@ function Sidebar({ children, title, handleGoToCart, MenuName = null }) {
           </header>
           <div
             className="content"
-            style={{
-              padding: isMobileDevice ? (activeMenu ? "0 0px 0px" : "0 20px") : "0 20px",
-            }}
+            // style={{
+            //   padding: isMobile ? (activeMenu ? "0 0px 0px" : "0 20px") : "0 20px",
+            // }}
           >
             {children}
           </div>
