c920affd5beb3e8d816308c178c0ed3aab88884e
Jay-abs <jay.shah@abs.in>
2025-12-26 11:42:47 +0530

    isMobile fix in catalog page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -45,12 +45,12 @@ function Sidebar({ children, title, MenuName = null }) {
   );
   //const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const [cartbranchData, setCartBranchData] = useState(null);
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
-    console.log("isMobile", isMobile);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  // useEffect(() => {
+  //   const handleResize = () => setIsMobile(window.innerWidth < 768);
+  //   console.log("isMobile", isMobile);
+  //   window.addEventListener("resize", handleResize);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
   const isMobile = usePlatform();
 
   const [isSidebarExpanded, setSidebarExpanded] = useState(false);
@@ -586,7 +586,7 @@ function Sidebar({ children, title, MenuName = null }) {
       isVisible: isMobile,
     },
   ];
-  const menuItems = isMobileDevice
+  const menuItems = isMobile
     ? [
         baseMenuItems.find((item) => item.label === "Dashboard"),
         baseMenuItems.find((item) => item.label === "Catalog"),
