48861835d5111bec06ac2768a11a5614eb21d15e
ABSDeveloper <developer@abs.in>
2026-02-05 18:09:59 +0530

    ios padding fixed ios

--- src/components/AddBankTransaction.js
+++ src/components/AddBankTransaction.js
@@ -87,6 +87,7 @@ const AddBankTransaction = () => {
   const cookieToken = getCookie("token");
   const isV = rbacMgr.isV.bind(rbacMgr);
   const isE = rbacMgr.isE.bind(rbacMgr);
+  const isIOSsMobile = /iPhone|iPad|iPod/i.test(navigator.userAgent);
   // const generateToken = async () => {
   //   try {
   //     const { data } = await axios.post(
@@ -542,7 +543,7 @@ const AddBankTransaction = () => {
   }, [])
   const renderTemplate = () => {
     return (
-      <div className="bank-add-container">
+      <div className={`bank-add-container  ${isIOSsMobile ? "ios-mobile-padding" : ""} ${isRTL ? "rtl" : ""}`}>
         <div className="bank-add-form">
           <div className="form-grid">
             <div className="form-group">
