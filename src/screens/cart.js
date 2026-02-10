b917a83810bccda827a086a66cfa7811a9f10685
Ashwin Sridhar <ashwin.sridhar@abs.in>
2026-02-10 17:40:47 +0530

    cart messages changed. order number displaying.

--- src/screens/cart.js
+++ src/screens/cart.js
@@ -1674,7 +1674,7 @@ function Cart() {
         Swal.fire({
           icon: "success",
           title: t("Your Request has been sent for Approval"),
-          text: t("Request sent with orders", {
+          text: t("Request sent with orders" + "#{{orders}}", {
             orders: orderIds.map((id) => `${id}`).join(", "),
             method: selectedPaymentMethod,
           }),
@@ -1977,7 +1977,7 @@ function Cart() {
             Swal.fire({
               icon: "success",
               title: t("Your Request has been sent for Approval"),
-              text: t("Request sent with order", { orders: machineOrderId }),
+              text: t("Request sent with order #{{orderId}}", { orderId: machineOrderId }),
               confirmButtonText: t("OK"),
             }).then(() => {
               fetchCartItems();
