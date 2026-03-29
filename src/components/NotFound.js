ecb0669e3f2f495fe0bfe5ec25908a43d078baaf
Jay-abs <jay.shah@abs.in>
2026-03-30 01:43:43 +0530

    Initial React with PBI Reports

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import "../styles/NotFound.css";
 
 const NotFound = () => {
   const navigate = useNavigate();
+const location = useLocation();
+
+  // ✅ Check if source is d365 from URL or sessionStorage
+  const params = new URLSearchParams(location.search);
+  const isFromD365 = params.get("source") === "d365" || 
+                     sessionStorage.getItem("accessSource") === "d365";
 
   const handleGoBack = () => {
     navigate(-1); // Go back to the previous page
@@ -17,14 +23,14 @@ const NotFound = () => {
         <p className="error-message-not-found">
           Sorry, the page you are looking for doesn't exist or has been moved.
         </p>
-        <div className="error-actions">
+        {!isFromD365 && (<div className="error-actions">
           <Link to="/catalog" className="btn-home">
             Go to Home
           </Link>
           <button onClick={handleGoBack} className="btn-back">
             Go Back
           </button>
-        </div>
+        </div>)}
       </div>
     </div>
   );
